44883cfda75e093d8c1775305aef7fef35a4db56
alterkim <alter@kakao.com>
2021-02-20 01:24:28 +0900

    Prepare for run node

--- x/nameservice/client/rest/queryWhois.go
+++ x/nameservice/client/rest/queryWhois.go
@@ -34,3 +34,18 @@ func getWhoisHandler(cliCtx context.CLIContext, storeName string) http.HandlerFu
 		rest.PostProcessResponse(w, cliCtx, res)
 	}
 }
+
+func resolveNameHandler(cliCtx context.CLIContext, storeName string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		paramType := vars["key"]
+
+		res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/%s/%s", storeName, types.QueryResolveName, paramType), nil)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusNotFound, err.Error())
+			return
+		}
+
+		rest.PostProcessResponse(w, cliCtx, res)
+	}
+}
--- x/nameservice/genesis.go
+++ x/nameservice/genesis.go
@@ -7,16 +7,26 @@ import (
 	// abci "github.com/tendermint/tendermint/abci/types"
 )
 
-// InitGenesis initialize default parameters
+// InitGenesis initialize default parameters (called on chain start)
 // and the keeper's address to pubkey map
 func InitGenesis(ctx sdk.Context, k keeper.Keeper /* TODO: Define what keepers the module needs */, data types.GenesisState) {
-	// TODO: Define logic for when you would like to initalize a new genesis
+	for _, record := range data.WhoisRecords {
+		keeper.SetWhois(ctx, record.Value, record)
+	}
 }
 
-// ExportGenesis writes the current store values
+// ExportGenesis writes the current store values (called after stopping the chain)
 // to a genesis file, which can be imported again
 // with InitGenesis
 func ExportGenesis(ctx sdk.Context, k keeper.Keeper) (data types.GenesisState) {
-	// TODO: Define logic for exporting state
-	return types.NewGenesisState()
+	var records []types.Whois
+	iterator := k.GetNamesIterator(ctx)
+	for ; iterator.Valid(); iterator.Next() {
+
+		name := string(iterator.Key())
+		whois, _ := k.GetWhois(ctx, name)
+		records = append(records, whois)
+
+	}
+	return types.GenesisState{WhoisRecords: records}
 }
