66c1a172536411c1cd13610badbbc2b31f670276
alterkim <alter@kakao.com>
2021-03-02 19:49:57 +0900

    Modify sh file

--- init.sh
+++ init.sh
@@ -3,24 +3,24 @@
 rm -rf ~/.cbdcd
 rm -rf ~/.cbdccli
 
-~/go/bin/cbdcd init test --chain-id=namechain
+cbdcd init test --chain-id=namechain
 
-~/go/bin/cbdccli config output json
-~/go/bin/cbdccli config indent true
-~/go/bin/cbdccli config trust-node true
-~/go/bin/cbdccli config chain-id namechain
-~/go/bin/cbdccli config keyring-backend test
+cbdccli config output json
+cbdccli config indent true
+cbdccli config trust-node true
+cbdccli config chain-id namechain
+cbdccli config keyring-backend test
 
-~/go/bin/cbdccli keys add user1
-~/go/bin/cbdccli keys add user2
+cbdccli keys add user1
+cbdccli keys add user2
 
-~/go/bin/cbdcd add-genesis-account $(~/go/bin/cbdccli keys show user1 -a) 1000nametoken,100000000stake
-~/go/bin/cbdcd add-genesis-account $(~/go/bin/cbdccli keys show user2 -a) 1000nametoken,100000000stake
+cbdcd add-genesis-account $(cbdccli keys show user1 -a) 1000cbdctoken,100000000stake
+cbdcd add-genesis-account $(cbdccli keys show user2 -a) 1000cbdctoken,100000000stake
 
-~/go/bin/cbdcd gentx --name user1 --keyring-backend test
+cbdcd gentx --name user1 --keyring-backend test
 
 echo "Collecting genesis txs..."
-~/go/bin/cbdcd collect-gentxs
+cbdcd collect-gentxs
 
 echo "Validating genesis file..."
-~/go/bin/cbdcd validate-genesis
+cbdcd validate-genesis
--- tx.sh
+++ tx.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-~/go/bin/cbdccli query account $(~/go/bin/cbdccli keys show user1 -a) | jq ".value.coins[0]"
-~/go/bin/cbdccli query account $(~/go/bin/cbdccli keys show user2 -a) | jq ".value.coins[0]"
+cbdccli query account $(cbdccli keys show user1 -a) | jq ".value.coins[0]"
+cbdccli query account $(cbdccli keys show user2 -a) | jq ".value.coins[0]"
 
 # Buy your first name using your coins from the genesis file
-~/go/bin/cbdccli tx cbdc buy-name user1.id 5cbdctoken --from user1 -y | jq ".txhash" |  xargs $(sleep 6) ~/go/bin/cbdccli q tx
+cbdccli tx cbdc buy-name user1.id 5cbdctoken --from user1 -y | jq ".txhash" |  xargs $(sleep 6) cbdccli q tx
 
 # Set the value for the name you just bought
-~/go/bin/cbdccli tx cbdc set-name user1.id 8.8.8.8 --from user1 -y | jq ".txhash" |  xargs $(sleep 6) ~/go/bin/cbdccli q tx
+cbdccli tx cbdc set-name user1.id 8.8.8.8 --from user1 -y | jq ".txhash" |  xargs $(sleep 6) cbdccli q tx
 
 # Try out a resolve query against the name you registered
-~/go/bin/cbdccli query cbdc resolve user1.id | jq ".value"
+cbdccli query cbdc resolve user1.id | jq ".value"
 # > 8.8.8.8
 
 # Try out a whois query against the name you just registered
-~/go/bin/cbdccli query cbdc get-whois user1.id
+cbdccli query cbdc get-whois user1.id
 # > {"value":"8.8.8.8","owner":"cosmos1l7k5tdt2qam0zecxrx78yuw447ga54dsmtpk2s","price":[{"denom":"nametoken","amount":"5"}]}
 
 # user2 buys name from user1
-~/go/bin/cbdccli tx cbdc buy-name user1.id 10cbdctoken --from user2 -y | jq ".txhash" |  xargs $(sleep 6) ~/go/bin/cbdccli q tx
+cbdccli tx cbdc buy-name user1.id 10cbdctoken --from user2 -y | jq ".txhash" |  xargs $(sleep 6) cbdccli q tx
 
 # user2 decides to delete the name she just bought from user1
-~/go/bin/cbdccli tx cbdc delete-name user1.id --from user2 -y | jq ".txhash" |  xargs $(sleep 6) ~/go/bin/cbdccli q tx
+cbdccli tx cbdc delete-name user1.id --from user2 -y | jq ".txhash" |  xargs $(sleep 6) cbdccli q tx
 
 # Try out a whois query against the name you just deleted
-~/go/bin/cbdccli query cbdc get-whois user1.id
+cbdccli query cbdc get-whois user1.id
 # > could not resolve whois user1.id 
